f8ce603b4977e1f8f6bd5be0293a29f3818f5ef4
mnyborg <manyb@dtu.dk>
2019-10-09 09:42:33 +0200

    20191008

--- lektion06/src/lektion06/Test.java
+++ lektion06/src/lektion06/Test.java
@@ -4,29 +4,88 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		int[] intArr = {0, 1, 2, 3, 4, 5};
+		int[] a = {1, 2, 3, 4, 5};
 
-		intArr[0] = 10;
+		System.out.println(avg(2.0,3.0));
+		System.out.println(avg(2.0,3.0,4.0));
+		System.out.println(avg(2.0,3.0,4.0,5.0));
 
-		//		for (int i = 0; i < intArr.length; i++)
-		//			System.out.println(intArr[i]);
 
-		//		for (int val : intArr)
-		//			System.out.println(val);
 
-		udskriv(intArr);
-		
+
+//		a = 5;
+//
+//		int jan = 31;
+//		int feb = 28;
+//		int mar = 31;
+//
+//		int[] monthLen = {31,28,31,30,31,30,31,31,30,31,30,31};
+//
+//		int[] intArr = new int[10];
+//
+//		for (int i = 0; i < intArr.length; i++) {
+//			intArr[i] = i*10;
+//
+//		}
+//
+//		for (int i = 0; i < intArr.length; i++) {
+//			System.out.println(intArr[i]);
+//			intArr[i] = intArr[i]-10;
+//
+//		}
+//
+//		System.out.println("for-each");
+//		for (int val : intArr) {
+//			System.out.println(val);
+//		}
+//		System.out.println();
+//		System.out.println();
+//
+//		String[] monthNames= {"jan","feb","mar", "apr", "maj"};
+//
+//
+////		intArr = new int[5];
+////
+////		intArr[0] = 5;
+////		intArr[1] = 6;
+////		intArr[2] = 7;
+////		intArr[3] = 8;
+////		intArr[4] = 9;
+////
+//
+//		for (int i = 0; i < monthLen.length; i++) {
+//			System.out.println(monthLen[i]);
+//		}
+//
+//		for (int i = 0; i < monthNames.length; i++) {
+//			System.out.println(monthNames[i]);
+//		}
 
 	}
 
+
+
+	public static double avg(double ... numbers) {
+		double total = 0.0; // initialiser total
+
+		// beregn sum af argumenter
+		for ( int i = 0; i < numbers.length; i++ )
+			total += numbers[i];
+
+		// returner gennemsnit
+		return total / numbers.length;
+
+
+
+
+	}
+
+
 	public static void udskriv(int[] arr) {
 
-		arr[2] = 100;
-		
+		// arr = a
 		for (int val : arr)
 			System.out.println(val);
-		
-		
 	}
 	
 	
