4c749785c35110710d6142e6975468675ca6cbc3
mnyborg <manyb@dtu.dk>
2019-10-29 14:57:52 +0100

    20191025

---
+++ lektion08/src/lektion08/Opgave.java
@@ -0,0 +1,18 @@
+package lektion08;
+
+public class Opgave {
+    public static void main(String[] args) {
+        String[][] kurser = {
+                {"01904", "01212", "02315"},
+                {"02324", "02326", "02327", "62577"},
+                {"02369", "62550"},
+                {"01920", "02346", "62410"}
+        };
+        int snr = 1;
+        for (String kurserStud[] : kurser) {
+            System.out.println(snr++ + ". semester");
+            for ( String kursus : kurserStud)
+                System.out.println(kursus);
+        }
+    }
+}
