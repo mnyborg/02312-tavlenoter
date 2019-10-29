e0e462d2e46c2c803e5dd1f3767779ef4b1de79f
mnyborg <manyb@dtu.dk>
2019-10-29 12:09:26 +0100

    20191029

--- lektion08/src/lektion08/Test.java
+++ lektion08/src/lektion08/Test.java
@@ -6,29 +6,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Dice[] diceArr = new Dice[3];
-		
-		diceArr[0] = new Dice(1);
-		diceArr[1] = new Dice(5);
-		diceArr[2] = new Dice(6);
-
-		// kast terninger
-		for (Dice d:diceArr) {
-			d.roll();
-		}
-
-		for(int i = 0; i < diceArr.length; i++)
-			System.out.println(diceArr[i]);
-
-
-		// to dim array
-
-		int[][] a =new int[3][];
-
-
-
-
 
 	}
 
