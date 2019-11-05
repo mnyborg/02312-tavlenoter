651c3aef2f2e5fc7b9436d0e89bb9435aa5528bf
mnyborg <manyb@dtu.dk>
2019-11-05 12:26:53 +0100

    20191105

--- lektion09/src/test/Test.java
+++ lektion09/src/test/Test.java
@@ -1,17 +1,10 @@
 package test;
 
-import lektion09.figurer.Cylinder;
 import lektion09.personer.Student;
 
-public class Test {
+public class Test  {
 
-	public static void main(String[] args) {
-		Student s = new Student("Pia Jensen", "Poppelvej 2", "s123456");
-		System.out.println(s);
-
-		Cylinder cy = new Cylinder(3.0, 4.0);
-		System.out.println(cy);
+    public static void main(String[] args) {
 
 	}
-
 }
