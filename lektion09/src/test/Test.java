6fa867d96724c98cc8aaaa314e2632bc72cb69fd
mnyborg <manyb@dtu.dk>
2019-11-12 11:45:45 +0100

    20191112

--- lektion09/src/test/Test.java
+++ lektion09/src/test/Test.java
@@ -1,17 +1,13 @@
 package test;
 
-import figurer.Cirkel;
-import figurer.Cylinder;
+import lektion09.figurer.Cylinder;
+import lektion09.personer.Student;
 
+public class Test  {
 
-public class Test {
-
-	public static void main(String[] args) {
-		Cylinder c = new Cylinder(3.0, 4.0);
-
-		System.out.println(c);
-		System.out.println("Cylinder areal: " + c.areal());
-
+    public static void main(String[] args) {
+        Cylinder c = new Cylinder(3.0,4.0);
+        System.out.println(c);
+        System.out.println(c.areal());
 	}
-
 }
