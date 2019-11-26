feb774a48bf6b720b1b38940548828dff583a9e3
mnyborg <manyb@dtu.dk>
2019-11-26 14:46:58 +0100

    20191126

--- lektion12/src/lektion12/WriteFileTest.java
+++ lektion12/src/lektion12/WriteFileTest.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.Random;
 
 public class WriteFileTest {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 		final int MAX = 10;
 
 
@@ -12,24 +12,33 @@ public class WriteFileTest {
 		String file = "lektion12/src/test.dat";
 		Random rand = new Random();
 
-		FileWriter fw = new FileWriter (file);
-		BufferedWriter bw = new BufferedWriter (fw);
-		PrintWriter outFile = new PrintWriter (bw);
+		PrintWriter outFile = null;
+		try {
+			FileWriter fw = new FileWriter (file);
+			BufferedWriter bw = new BufferedWriter (fw);
+			outFile = new PrintWriter(bw);
 
 
-		for (int line=1; line <= MAX; line++)
-		{
-			for (int num=1; num <= MAX; num++)
-			{	
-				value = rand.nextInt (90) + 10;
-				outFile.print (value + "   ");
-			}
-			outFile.println ();
+			for (int line=1; line <= MAX; line++)
+            {
+                for (int num=1; num <= MAX; num++)
+                {
+                    value = rand.nextInt (90) + 10;
+                    bw.write (value + "   ");
+                }
+                bw.write ("\n");
+            }
+		} catch (IOException e) {
+		//	e.printStackTrace();
+			System.out.println("Fejl i kommunikation med fil - kontakt administrator");
 		}
 
 
-		outFile.close();
-		System.out.println ("Output file has been created: " + file);
+		if (outFile != null) {
+			System.out.println ("Output file has been created: " + file);
+			outFile.close();
+		}
+
 	}
 
 }
