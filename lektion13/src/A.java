c04b61f6a2084321829ad123ac7bc4b665ffdcb1
mnyborg <manyb@dtu.dk>
2020-12-04 09:41:42 +0100

    20201204

--- lektion13/src/A.java
+++ lektion13/src/A.java
@@ -1,6 +1,14 @@
+/**
+ * class A handler om ....
+ * @author Mads Nyborg
+ */
 public class A {
     private int field1;
 
+    /**
+     * Konstruktør
+     * @param field1 field1 er ...
+     */
     public A(int field1) {
         this.field1 = field1;
     }
@@ -9,6 +17,11 @@ public class A {
     // 2. betingelse tester at other er en instans af A
     // 3. betingelse tester at other.field == this.field
 
+    /**
+     * Implementering af equals
+     * @param other objekt de skal sammenlignes med
+     * @return true hvis ens, ellers false
+     */
     @Override
     public boolean equals(Object other) {
         return (other != null
